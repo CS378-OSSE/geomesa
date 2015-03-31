b81eaa749584f8c7eff24188146b938e4600f44b
Kevin Wheeler <kevinwheeler90@gmail.com>
2015-03-30 22:45:09 -0500

    CQL partially working
    TODO:
    CQL can't be unapplied by clearing the input text box.
    Getting some chrome console error that doesn't
    seem to matter, but we should still get rid of it.

--- geomesa-ui/src/main/ui/src/app/map/map.js
+++ geomesa-ui/src/main/ui/src/app/map/map.js
@@ -9,23 +9,6 @@ angular.module('geomesa.map', [])
                 api: '=',
                 selectedFeatures: '=',
             },
-            link: function (scope, element, attrs) {
-                //var baseLayer = L.tileLayer.provider('Stamen.TonerLite'),
-                var baseLayer = L.tileLayer.provider('MapQuestOpen.OSM'), 
-                    wmsLayer = L.tileLayer.wms("http://geomesa:8080/geoserver/geomesa/wms", {
-                        layers: 'geomesa:QuickStart',
-                        format: 'image/png',
-                        transparent: true
-                    });
-
-                scope.map = L.map(element[0], {
-                    center: L.latLng(-38.09, -76.85),
-                    zoom: 8,
-                    maxZoom: 18,
-                    minZoom: 3,
-                    attributionControl: false,
-                    layers: [baseLayer, wmsLayer]
-                });
 
             link: function (scope, element, attrs) {
                 scope.selectedFeatures = [
@@ -63,7 +46,9 @@ angular.module('geomesa.map', [])
 
                 scope.api = {
                     applyCQL: function (cql) {
-                        console.log(cql);
+                        var params = {CQL_FILTER: cql};
+                        wmsSource.updateParams(params);
+                        console.log("cql applied: " + cql);
                     }
                 };
 
